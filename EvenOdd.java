4c9e53bd2401027bf495bc5d7915f45212eb50eb
AkariMichiko <49076291+AkariMichiko@users.noreply.github.com>
2019-03-30 19:04:38 +0600

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+package Basic;
+import java.util.*;
+public class EvenOdd {
+	public static void main(String [] args) {
+		Scanner input= new Scanner(System.in);
+		
+		System.out.print("Enter n: ");
+		int n = input.nextInt();
+		
+		if(n%2==0) {
+			System.out.print("Even");
+		}
+		else {
+			System.out.print("Odd");
+		}
+	}
+
+}
---
+++ GCD.java
@@ -0,0 +1,22 @@
+package Loops;
+import java.util.*;
+public class GCD {
+	public static void main(String [] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter n1: ");
+		int n1=input.nextInt();
+		
+		System.out.print("Enter n2: ");
+		int n2=input.nextInt();
+		
+		int sum=0;
+		while(n2!=0) {
+			sum=n1%n2;
+			n1=n2;
+			n2=sum;
+		}
+		System.out.println("GCD is: "+n1);
+
+}	
+}	
