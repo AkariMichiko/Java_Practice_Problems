38edf807beba0c8d01fcc170b1e129da7f8aac6b
AkariMichiko <49076291+AkariMichiko@users.noreply.github.com>
2019-03-30 19:10:18 +0600

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -3,13 +3,10 @@ import java.util.*;
 public class GCD {
 	public static void main(String [] args) {
 		Scanner input = new Scanner(System.in);
-		
 		System.out.print("Enter n1: ");
 		int n1=input.nextInt();
-		
 		System.out.print("Enter n2: ");
 		int n2=input.nextInt();
-		
 		int sum=0;
 		while(n2!=0) {
 			sum=n1%n2;
@@ -17,6 +14,5 @@ public class GCD {
 			n2=sum;
 		}
 		System.out.println("GCD is: "+n1);
-
-}	
+	}	
 }	
