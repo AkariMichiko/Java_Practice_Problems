52e28163b4750b514e7f666eb726f38b5f116aa5
AkariMichiko <49076291+AkariMichiko@users.noreply.github.com>
2019-03-30 19:07:59 +0600

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,15 +3,12 @@ import java.util.*;
 public class MultiplicationTable {
 	public static void main (String [] args) {
 		Scanner input = new Scanner(System.in);
-		
 		System.out.print("Enter n: ");
 		int n=input.nextInt();
-		
 		int sum;
 		for(int i=1; i<=10; i++) {
 			sum=i*n;
 			System.out.println(sum);
 		}
 	}
-
 }
